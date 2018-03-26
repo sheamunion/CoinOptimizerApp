36a4a77de1dbb462dff9867f3b58e1bab4d189a3
Shea Munion <sheamunion@gmail.com>
2018-03-26 15:27:09 -0500

    Initial commit.

---
+++ src/com/sheamunion/CoinOptimizer/Main.java
@@ -0,0 +1,11 @@
+package com.sheamunion.CoinOptimizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Welcome! Get the optimal coin combination for a dollar value.");
+            System.exit(1);
+        }
+    }
+}
